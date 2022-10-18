c6b3bb60c90a938e2785086823765f5020536224
ArnoldAyala <114626652+ArnoldAyala@users.noreply.github.com>
2022-10-18 16:21:33 -0700

    Add files via upload

--- Activity 6/income_tax/calculate_tax.js
+++ Activity 6/income_tax/calculate_tax.js
@@ -14,23 +14,23 @@ function calculateTax(inc) {
 	if (inc <= 9875) {
 		$("#tax").value = inc * .10;
 	}
-	else if (inc <= 40125) {
-		$("#tax").value = inc * .12 + 987.50;
+	else if (inc > 9875 && inc <= 40125) {
+		$("#tax").value = 987.50 + (inc - 9875) * .12;
 	}
-	else if (inc <= 85525) {
-		$("#tax").value = inc * .22 + 4617.50;
+	else if (inc > 40125 && inc <= 85525) {
+		$("#tax").value = 4617.50 + (inc - 40125) * .22;
 	}
-	else if (inc <= 163300) {
-		$("#tax").value = inc * .24 + 14605.50;
+	else if (inc > 85525 && inc <= 163300) {
+		$("#tax").value = 14605.50 + (inc - 85525) * .24;
 	}
-	else if (inc <= 207350) {
-		$("#tax").value = inc * .32 + 33271.50;
+	else if (inc > 163300 && inc <= 207350) {
+		$("#tax").value = 33271.50 + (inc - 163300) * .32;
 	}
-	else if (inc <= 518400) {
-		$("#tax").value = inc * .35 + 47367.50;
+	else if (inc > 207350 && inc <= 518400) {
+		$("#tax").value = 47367.50 + (inc - 207350) * .35;
 	}
 	else {
-		$("#tax").value = inc * .37 + 156235.00;
+		$("#tax").value = 156235.00 + (inc - 518400) * .37;
 	}
 }
 
